cdcfb943479413a8425a28bf25a7a18cd0517cff
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2017-03-19 18:57:44 +1100

    Prettify results

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const sinon = require('sinon')
+const Table = require('easy-table')
 const TrafficLightIntersection = require('./traffic-light')
 const TrafficLightIntersectionController = require('./traffic-light-controller')
 const { TrafficLights } = require('./traffic-light.models')
@@ -10,37 +11,47 @@ const config = {
   yellowDuration: 1000 * 30    // Yellow light is on for 30 seconds before switching to red
 }
 
-function outputState(state) {
-  console.log(`North: ${state[TrafficLights.North]}`)
-  console.log(`East: ${state[TrafficLights.East]}`)
-  console.log(`South: ${state[TrafficLights.South]}`)
-  console.log(`West: ${state[TrafficLights.West]}\n`)
+let i = 0;
+function getResult(state) {
+  return {
+    '#': i++,
+    'Time': `${Date.now() / 1000 / 60} min`,
+    'North': state[TrafficLights.North],
+    'East': state[TrafficLights.East],
+    'South': state[TrafficLights.South],
+    'West': state[TrafficLights.West]
+  }
 }
-
 // Create an intersection using default config provided from the factory.
 const intersection = TrafficLightIntersection.of()
 
-// Use a fake timer so that the simulation can be fast forwarded to particular points in time. This enables the 
+// Use a fake timer so that the simulation can be fast forwarded to particular points in time. This enables the
 // simulation to be run synchronously. A real timer could be implemented and used instead.
 const clock = sinon.useFakeTimers()
+
+// Results output
+const results = []
 
 // Instantiate the controller with the necessary parameters.
 const controller = TrafficLightIntersectionController.of(intersection, {
   clock,
   onTrafficLightChange: (state) => {
-    console.log(`Elapsed: ${Date.now() / 1000} seconds`)
-    outputState(state)
+    results.push(getResult(state))
   },
   turnDuration: config.turnDuration,
   yellowDuration: config.yellowDuration
 })
 
-// Output the initial configuration.
-console.log('Starting simulation\n')
-console.log('Starting configuration:')
-outputState(intersection.getState())
+console.log('Starting simulation...')
+results.push(getResult(intersection.getState()))
 
 // Start the simulation, fast forward it by the duration config (30 minutes) and then stop the simulation.
 controller.start()
 clock.tick(config.duration)
 controller.stop()
+
+console.log('Simulation finished\n')
+
+console.log('Simulation Results\n')
+console.log('==================\n')
+console.log(Table.print(results))
