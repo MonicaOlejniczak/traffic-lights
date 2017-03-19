7bba971f3ec7d89e15d0adc05daaf08183dc567b
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2017-03-19 15:54:28 +1100

    Create traffic light models

---
+++ src/traffic-light.models.js
@@ -0,0 +1,17 @@
+const TrafficLights = {
+  North: 'North',
+  East: 'East',
+  South: 'South',
+  West: 'West'
+}
+
+const TrafficLightColor = {
+  Green: 'Green',
+  Yellow: 'Yellow',
+  Red: 'Red'
+}
+
+module.exports = {
+  TrafficLights,
+  TrafficLightColor
+}
