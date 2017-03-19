8fd0507854d78d7d92982714a31afef941a7867d
Monica Olejniczak <monica.j.olejniczak@gmail.com>
2017-03-19 17:04:35 +1100

    Fix tests

--- src/traffic-light-controller.spec.js
+++ src/traffic-light-controller.spec.js
@@ -1,41 +1,52 @@
-const expect = require('chai').expect
-const sinon = require('sinon');
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
 
-const { TrafficLights, TrafficLightColor, TrafficLightIntersection } = require('./traffic-lights')
+const { expect } = chai
+chai.use(sinonChai)
+
+const { TrafficLights, TrafficLightColor } = require('./traffic-light.models')
+const TrafficLightIntersection = require('./traffic-light')
+const TrafficLightIntersectionController = require('./traffic-light-controller')
 
 const { Green, Yellow, Red } = TrafficLightColor;
 
 describe('traffic lights', function() {
 
-  function expectTrafficColors(intersection, north, east, south, west) {
-    expect(intersection.getTrafficLightColors()).to.deep.equal({
-      [TrafficLights.North]: north,
-      [TrafficLights.East]: east,
-      [TrafficLights.South]: south,
-      [TrafficLights.West]: west
+  function createIntersectionController(callback) {
+    const intersection = new TrafficLightIntersection({
+      [TrafficLights.North]: TrafficLightColor.Green,
+      [TrafficLights.East]: TrafficLightColor.Red,
+      [TrafficLights.South]: TrafficLightColor.Green,
+      [TrafficLights.West]: TrafficLightColor.Red
     })
-  }
 
-  function createIntersection(callback) {
     const config = {
+      clock: sinon.useFakeTimers(),
       turnDuration: 1000 * 60 * 5,
       yellowDuration: 1000 * 30,
       callback
     }
-    return { config, intersection: TrafficLightIntersection.of(config) }
+
+    const controller = new TrafficLightIntersectionController(intersection, config)
+
+    return { config, controller }
   }
 
-  it('should start with north and south as green and east and west as red', function () {
-    const { intersection } = createIntersection();
-    expectTrafficColors(intersection, Green, Red, Green, Red);
-  })
+  // it('should start with north and south as green and east and west as red', function () {
+  //   const { intersection } = createIntersection();
+  //   expectTrafficColors(intersection, Green, Red, Green, Red);
+  // })
 
   it('should automatically change the lights from green to red every five minutes', function () {
     const spy = sinon.spy()
-    const { config, intersection } = createIntersection(spy)
-    intersection.tick(config.turnDuration)
+    const { config, controller } = createIntersectionController(spy)
 
-    expect(spy).to.be.calledTwice() // Green -> Yellow -> Red
+    controller.start()
+    config.clock.tick(config.turnDuration)
+    controller.stop()
+
+    expect(spy).to.be.calledTwice // Green -> Yellow -> Red
     expect(spy).to.be.calledWith({
       [TrafficLights.North]: Red,
       [TrafficLights.East]: Green,
@@ -46,18 +57,24 @@ describe('traffic lights', function() {
 
   it('should not change traffic light colors one second before (turnDuration - yellowDuration)', function () {
     const spy = sinon.spy()
-    const { config, intersection } = createIntersection(spy)
-    intersection.tick(config.turnDuration - config.yellowDuration - 1)
+    const { config, controller } = createIntersectionController(spy)
 
-    expect(spy).to.be.not.be.called()
+    controller.start()
+    config.clock.tick(config.turnDuration - config.yellowDuration - 1)
+    controller.stop()
+
+    expect(spy).to.be.not.be.called
   })
 
   it('should change the lights from green to yellow and the other remain red at (turnDuration - yellowDuration)', function () {
     const spy = sinon.spy()
-    const { config, intersection } = createIntersection(spy)
-    intersection.tick(config.turnDuration - config.yellowDuration)
+    const { config, controller } = createIntersectionController(spy)
 
-    expect(spy).to.be.calledOnce() // Green -> Yellow
+    controller.start()
+    config.clock.tick(config.turnDuration - config.yellowDuration)
+    controller.stop()
+
+    expect(spy).to.be.calledOnce // Green -> Yellow
     expect(spy).to.be.calledWith({
       [TrafficLights.North]: Yellow,
       [TrafficLights.East]: Red,
@@ -68,9 +85,11 @@ describe('traffic lights', function() {
 
   it('should display the red light for yellow duration prior to switching to red', function () {
     const spy = sinon.spy()
-    const { config, intersection } = createIntersection()
-    
-    intersection.tick(config.turnDuration - config.yellowDuration)
+    const { config, controller } = createIntersectionController(spy)
+
+    controller.start()
+
+    config.clock.tick(config.turnDuration - config.yellowDuration)
     expect(spy).to.be.calledWith({
       [TrafficLights.North]: Yellow,
       [TrafficLights.East]: Red,
@@ -78,18 +97,17 @@ describe('traffic lights', function() {
       [TrafficLights.West]: Red
     })
 
-    intersection.tick(config.yellowDuration)
+    config.clock.tick(config.yellowDuration)
     expect(spy).to.be.calledWith({
       [TrafficLights.North]: Red,
       [TrafficLights.East]: Green,
       [TrafficLights.South]: Red,
       [TrafficLights.West]: Green
     })
-    
-    expect(spy).to.be.calledTwice()
-  })
 
-  it('should run for thirty minutes', function () {
+    controller.stop()
+
+    expect(spy).to.be.calledTwice
 
   })
 
